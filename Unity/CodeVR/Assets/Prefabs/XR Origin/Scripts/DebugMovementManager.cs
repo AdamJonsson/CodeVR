6155f3fd61b256bbe2d908e0303179a31a1c8eb0
Adam Jonsson <adamsmajl@gmail.com>
2022-05-05 11:15:09 +0200

    feat: Disable movement by using joystick

---
+++ Unity/CodeVR/Assets/Prefabs/XR Origin/Scripts/DebugMovementManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class DebugMovementManager : MonoBehaviour
+{
+    [SerializeField] private InputAction _enableMovementInput;
+    private ContinuousMoveProviderBase _movementScript;
+    private SnapTurnProviderBase _turnScript;
+
+    void Awake()
+    {
+        this._movementScript = GetComponent<ContinuousMoveProviderBase>();
+        this._turnScript = GetComponent<SnapTurnProviderBase>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _enableMovementInput.performed += this.OnToggleMovement;
+        _enableMovementInput.Enable();
+    }
+
+    private void OnToggleMovement(InputAction.CallbackContext context)
+    {
+        this._movementScript.enabled = !this._movementScript.enabled;
+        this._turnScript.enabled = !this._turnScript.enabled;
+    }
+
+}
