afdd62b2e5deb0798879e69eb1572b1f990f248a
Martin Hedlund <martinhedlund@live.com>
2022-12-09 16:04:56 +0100

    Update docker fixes

--- Website/Frontend/createConfig.js
+++ Website/Frontend/createConfig.js
@@ -2,29 +2,40 @@ const os = require("os");
 const fs = require("fs");
 require("dotenv").config();
 
-const ifaces = os.networkInterfaces();
+console.log("NODE ENV: ");
+console.log(process.env.NODE_ENV);
 
-// Iterate over interfaces ...
-const adresses = Object.values(ifaces).reduce((acc, iface) => {
-  const targetAdresses = iface
-    .filter((adress) => adress.family === "IPv4" && !adress.internal)
-    .map((a) => a.address);
-  return acc.concat(targetAdresses);
-}, []);
-
-console.log(adresses);
-
-process.env.NODE_ENV = "production";
-
-if (adresses[0] !== process.env.REACT_APP_ADRESS) {
-  console.log("Local IP has changes, updating the .evn");
-  if (process.env.NODE_ENV === "production") {
-    console.log("In production, creating ENV variable");
+if (process.env.NODE_ENV === "production") {
+  console.log("Creating config for production");
+  if (process.env.REACT_APP_HEROKU_BASE_URL) {
+    console.log(
+      "Setting ENV REACT_APP_HEROKU_BASE_URL to: " +
+        process.env.REACT_APP_HEROKU_BASE_URL
+    );
     process.env.REACT_APP_ADRESS = process.env.REACT_APP_HEROKU_BASE_URL;
   } else {
+    console.log(
+      "REACT_APP_HEROKU_BASE_URL is missing. Please specify in docker-compose and rebuild "
+    );
+    process.exit(1);
+  }
+} else {
+  // Iterate over interfaces ...
+
+  const ifaces = os.networkInterfaces();
+
+  const adresses = Object.values(ifaces).reduce((acc, iface) => {
+    const targetAdresses = iface
+      .filter((adress) => adress.family === "IPv4" && !adress.internal)
+      .map((a) => a.address);
+    return acc.concat(targetAdresses);
+  }, []);
+
+  console.log("Local IP Adress of Server: ");
+  console.log(adresses);
+
+  if (adresses[0] !== process.env.REACT_APP_ADRESS) {
+    console.log("Local IP has changes, updating the .evn and writing to file");
     fs.writeFileSync(__dirname + "/.env", "REACT_APP_ADRESS=" + adresses[0]);
   }
 }
-
-console.log("Local IP Adress of Server: ");
-console.log(adresses);
